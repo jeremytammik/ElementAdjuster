84ba76e0664ba0f63f778fbfd471c3107e37ce27
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-11-14 10:05:45 +0100

    CmdImport skeleton

--- ElementAdjuster/CmdImport.cs
+++ ElementAdjuster/CmdImport.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿#region Namespaces
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+#endregion
 
 namespace ElementAdjuster
 {
-  class CmdImport
+  [Transaction( TransactionMode.Manual )]
+  public class CmdImport : IExternalCommand
   {
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements )
+    {
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Application app = uiapp.Application;
+      Document doc = uidoc.Document;
+      Selection sel = uidoc.Selection;
+
+
+      return Result.Succeeded;
+    }
   }
 }
